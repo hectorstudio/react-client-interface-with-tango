dbe8c49fd202893c2d7aa44ca03e4beca9ccabfb
hectorstudio <david.liu987@hotmail.com>
2018-05-14 14:34:01 +0200

    NEw files

--- src/reducers/deviceList.js
+++ src/reducers/deviceList.js
@@ -1,12 +1,12 @@
 import initialState from './initialState';
 import {SET_DEVICES, SET_HIGHLIGHTED_DEVICE} from '../actions/actionTypes';
 
-export default function deviceList(state = initialState.devices, action) {
+export default function deviceList(state = initialState.deviceList, action) {
   switch (action.type) {
     case SET_DEVICES:
       return {...state, devices: action.list.output};
     case SET_HIGHLIGHTED_DEVICE:
-      return {...state, highlightedDevice: action.info};
+      return {...state, highlightedDevice: action.info.name};
     default:
       return state;
   }
