16c4b19f8815c9fe8f91a91d391532ea1572ec40
Hannes Petri <hannes.petri@maxiv.lu.se>
2018-07-25 20:03:35 +0200

    Added selectors for current device

--- src/selectors/currentDevice.ts
+++ src/selectors/currentDevice.ts
@@ -19,3 +19,30 @@ export const getCurrentDeviceProperties = createSelector(
     getCurrentDevicePropertiesState,
     state => state
 );
+
+const getCurrentDeviceState = (state: IRootState) => state.currentDevice;
+const getAttributesState = (state: IRootState) => state.attributes;
+const getCommandsState = (state: IRootState) => state.commands;
+const getPropertiesState = (state: IRootState) => state.properties;
+
+function objectValues(obj) {
+    return Object.keys(obj).map(key => obj[key]);
+}
+
+export const getCurrentDeviceAttributes2 = createSelector(
+    getAttributesState,
+    getCurrentDeviceState,
+    (attributes, current) => objectValues(attributes[current])
+);
+
+export const getCurrentDeviceCommands2 = createSelector(
+    getCommandsState,
+    getCurrentDeviceState,
+    (commands, current) => objectValues(commands[current])
+);
+
+export const getCurrentDeviceProperties2 = createSelector(
+    getPropertiesState,
+    getCurrentDeviceState,
+    (properties, current) => objectValues(properties[current])
+);
