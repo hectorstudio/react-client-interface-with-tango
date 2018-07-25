9a642c63cbcef7c4ee19e0aa90870befc8600d9f
hectorstudio <david.liu987@hotmail.com>
2018-07-25 20:58:13 +0200

    Cleaned up root reducer

--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -5,9 +5,6 @@ import deviceDetail, { IDeviceDetailState } from './deviceDetail';
 import deviceList, { IDeviceListState } from './deviceList';
 import loadingStatus, { ILoadingStatusState } from './loadingStatus';
 import commandOutput, { ICommandOutputState } from './commandOutput';
-import currentDeviceInfo, { ICurrentDeviceInfoState } from './currentDeviceInfo';
-import currentDeviceProperties, { ICurrentDevicePropertiesState } from './currentDeviceProperties';
-// import currentDeviceAttributes, { ICurrentDeviceAttributesState } from './currentDeviceAttributes';
 
 import currentDevice from './currentDevice';
 import devices2, { IDevices2State } from './devices2';
@@ -18,16 +15,14 @@ import properties, { IPropertiesState } from './properties';
 import ui from './ui';
 
 export interface IRootState {
-  devices: IDevicesState;
+  // View state
   deviceDetail: IDeviceDetailState;
   deviceList: IDeviceListState;
+  
+  // Data state
+  devices: IDevicesState;
   loadingStatus: ILoadingStatusState;
   commandOutput: ICommandOutputState;
-  
-  currentDeviceInfo: ICurrentDeviceInfoState;
-  currentDeviceProperties: ICurrentDevicePropertiesState;
-  // currentDeviceAttributes: ICurrentDeviceAttributesState,
-
   currentDevice: string;
   devices2: IDevices2State,
   attributes: IAttributesState;
@@ -36,21 +31,17 @@ export interface IRootState {
 }
 
 const rootReducer = combineReducers<IRootState>({
-  devices,
   deviceDetail,
   deviceList,
+  devices,
   loadingStatus,
   commandOutput,
-  currentDeviceInfo,
-  currentDeviceProperties,
-  // currentDeviceAttributes,
-  ui,
-
   currentDevice,
   devices2,
   attributes,
   commands,
   properties,
+  ui,
 });
 
 export default rootReducer;
