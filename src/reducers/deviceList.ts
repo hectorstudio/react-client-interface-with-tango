a8231bffbb5b52d11165254ed894eb705b6a182a
Hannes Petri <hannes.petri@maxiv.lu.se>
2018-07-25 16:06:49 +0200

    Removed loading status from deviceList

--- src/reducers/deviceList.ts
+++ src/reducers/deviceList.ts
@@ -1,5 +1,4 @@
 import {
-    FETCH_DEVICE_NAMES,
     FETCH_DEVICE_NAMES_SUCCESS,
     SET_SEARCH_FILTER,
 } from '../actions/actionTypes';
@@ -7,20 +6,15 @@ import {
 export interface IDeviceListState {
     filter: string;
     nameList: string[];
-    loadingNames: boolean;
 }
 
 export default function deviceList(state: IDeviceListState = {
     filter: '',
     nameList: [],
-    loadingNames: false,
 }, action) {
     switch (action.type) {
-    case FETCH_DEVICE_NAMES:
-        return {...state, loadingNames: true};
-
     case FETCH_DEVICE_NAMES_SUCCESS:
-        return {...state, loadingNames: false, nameList: action.names};
+        return {...state, nameList: action.names};
     
     case SET_SEARCH_FILTER:
         return {...state, filter: action.filter};  
