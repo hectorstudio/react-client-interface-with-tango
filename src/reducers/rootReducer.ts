5ee0c59bc26961dbb9e71aa2d41f372255eb0d63
Hannes Petri <hannes.petri@maxiv.lu.se>
2018-07-25 16:01:42 +0200

    Added loadingStatus reducer to root reducer

--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -3,18 +3,21 @@ import {combineReducers} from 'redux';
 import devices, { IDevicesState } from './devices';
 import deviceView, { IDeviceViewState } from './deviceView';
 import deviceList, { IDeviceListState } from './deviceList';
+import loadingStatus, { ILoadingStatusState } from './loadingStatus';
 import ui from './ui';
 
 export interface IRootState {
   devices: IDevicesState,
   deviceView: IDeviceViewState,
   deviceList: IDeviceListState,
+  loadingStatus: ILoadingStatusState,
 }
 
 const rootReducer = combineReducers<IRootState>({
   devices,
   deviceView,
   deviceList,
+  loadingStatus,
   ui,
 });
 
