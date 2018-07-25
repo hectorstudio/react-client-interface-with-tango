36ab0016ea03d3d51c1534e5e61644a0f22074de
Hannes Petri <hannes.petri@maxiv.lu.se>
2018-07-25 14:09:06 +0200

    Added deviceView reducer to root reducer

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -3,9 +3,11 @@ import {combineReducers} from 'redux';
 import devices from './devices';
 import filtering from './filtering';
 import ui from './ui';
+import deviceView from './deviceView';
 
 const rootReducer = combineReducers({
   devices,
+  deviceView,
   filtering,
   ui,
 });
