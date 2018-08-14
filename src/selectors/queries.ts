1776e57a7811387675009322ef4246a245d76e58
Hannes Petri <hannes.petri@maxiv.lu.se>
2018-08-14 13:55:57 +0200

    Attempt at solving device selection issue by reconstructing device object when selecing

--- src/selectors/queries.ts
+++ src/selectors/queries.ts
@@ -1,5 +1,15 @@
 import { IRootState } from '../reducers/rootReducer';
+import { objectValues } from '../utils';
 
 export function queryDeviceWithName(state: IRootState, name: string) {
-    return state.devices[name];
+    const device = state.devices[name];
+    if (device == null) {
+        return device;
+    }
+
+    const properties = [...objectValues(state.properties[name])];
+    const attributes = [...objectValues(state.attributes[name])];
+    const commands = [...objectValues(state.commands[name])];
+    
+    return {...device, properties, attributes, commands};
 }
