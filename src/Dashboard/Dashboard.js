bf6c03da9ca1f9256ee4ee881aecf614eb4763d8
hectorstudio <david.liu987@hotmail.com>
2018-10-01 15:18:53 +0200

    Some work on widget wrapper

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -1,25 +1,57 @@
 import React, { Component } from 'react';
 
 import './Dashboard.css';
+import { connect } from 'react-redux';
+import { subscribeDevice } from '../actions/tango';
 
 const WIDGETS = [
-    {x: 10, y: 10, type: 'COMMAND'},
-    {x: 40, y: 100, type: 'COMMAND'},
-    {x: 100, y: 100, type: 'COMMAND'},
+    {type: 'ATTRIBUTE_READ_ONLY', x: 10, y: 10, device: 'sys/tg_test/1', attribute: 'ampli'},
+    // {x: 40, y: 100, type: 'COMMAND'},
+    // {x: 100, y: 200, type: 'COMMAND'},
 ];
+
+class WidgetWrapper extends Component {
+    componentDidMount() {
+        const {device, attribute} = this.props;
+        const model = `${device}/${attribute}`;
+        this.subscribe(model);
+    }
+
+    render() {
+        return (
+            <div>
+                { this.props.children }
+            </div>
+        );
+    }
+}
 
 function widgetToComponent(widget) {
     return (
         <div className="Widget" style={{left: widget.x, top: widget.y}}>
-            {widget.type}
+            <WidgetWrapper device={widget.device} attribute={widget.attribute}>
+                {widget.type}
+            </WidgetWrapper>
         </div>
     );
 }
 
-export default class Dashboard extends Component {
+class Dashboard extends Component {
     render() {
         return <div className="Dashboard">
             {WIDGETS.map(widgetToComponent)}
         </div>;
     }
 }
+
+function mapStateToProps() {
+    return {};
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        subscribe: model => dispatch(subscribeDevice)
+    };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Dashboard);
