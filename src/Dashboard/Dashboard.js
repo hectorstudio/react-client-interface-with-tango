89dd0352a331c2595fadeb712f9a146287960910
hectorstudio <david.liu987@hotmail.com>
2018-10-05 11:49:58 +0200

    Added select control for devices in inspector

--- src/Dashboard/Dashboard.js
+++ src/Dashboard/Dashboard.js
@@ -7,107 +7,13 @@ import queryString from "query-string";
 import EditCanvas from "./EditCanvas/EditCanvas";
 import Library from "./Library/Library";
 import RunCanvas from "./RunCanvas/RunCanvas";
+import Inspector from "./Inspector/Inspector";
 
 import { WIDGET_DEFINITIONS, getWidgetDefinition } from "./widgetDefinitions";
 
 import "./Dashboard.css";
 
-class Inspector extends Component {
-  inputForParam(param, value) {
-    const type = this.props.widget.type;
-    const widgetDefinition = getWidgetDefinition(type);
-    const paramDefinition = widgetDefinition.params.find(
-      paramDef => paramDef.name === param
-    );
 
-    switch (paramDefinition.type) {
-      case "boolean":
-        return (
-          <input
-            type="checkbox"
-            checked={value}
-            onChange={e => this.props.onParamChange(param, e.target.checked)}
-          />
-        );
-      case "string":
-        return (
-          <input
-            type="text"
-            value={value}
-            onChange={e => this.props.onParamChange(param, e.target.value)}
-          />
-        );
-      case "number":
-        return (
-          <input
-            type="text"
-            value={value}
-            onChange={e =>
-              this.props.onParamChange(param, Number(e.target.value))
-            }
-          />
-        );
-      default:
-        return <input type="text" />;
-    }
-  }
-
-  render() {
-    const { type, params, device, attribute } = this.props.widget;
-    const definition = getWidgetDefinition(type);
-    const fields = definition.fields;
-    const paramDefinitions = definition.params;
-
-    return (
-      <div className="Inspector">
-        <h1>Inspector</h1>
-        {fields.length > 0 && (
-          <table>
-            <tbody>
-              {fields.indexOf("device") !== -1 && (
-                <tr>
-                  <td>Device:</td>
-                  <td>
-                    <input
-                      type="text"
-                      value={device}
-                      onChange={e => this.props.onDeviceChange(e.target.value)}
-                    />
-                  </td>
-                </tr>
-              )}
-              {fields.indexOf("attribute") !== -1 && (
-                <tr>
-                  <td>Attribute:</td>
-                  <td>
-                    <input
-                      type="text"
-                      value={attribute}
-                      onChange={e =>
-                        this.props.onAttributeChange(e.target.value)
-                      }
-                    />
-                  </td>
-                </tr>
-              )}
-            </tbody>
-          </table>
-        )}
-        {fields.length > 0 && <hr />}
-        <table>
-          <tbody>
-            {paramDefinitions.map(({ name, description }) => (
-              <tr key={name}>
-                <td>{description || name}: </td>
-                <td>{this.inputForParam(name, params[name])}</td>
-              </tr>
-            ))}
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-}
 
 class Dashboard extends Component {
   constructor(props) {
@@ -121,6 +27,7 @@ class Dashboard extends Component {
       sidebar: "library", // Belongs in edit component
       selectedWidgetIndex: -1, // Belongs in edit component
       widgets,
+      deviceNames: []
     };
 
     this.toggleMode = this.toggleMode.bind(this);
@@ -131,6 +38,31 @@ class Dashboard extends Component {
     this.handleParamChange = this.handleParamChange.bind(this);
     this.handleDeviceChange = this.handleDeviceChange.bind(this);
     this.handleAttributeChange = this.handleAttributeChange.bind(this);
+  }
+
+  componentDidMount() {
+    const client = require("graphql-client")({
+      url: `/db`
+    });
+
+    function callServiceGraphQL(query, variables) {
+      return client.query(query, variables || {}, (req, res) => {
+        if (res.status === 401) {
+          throw new Error("Not authorized");
+        }
+      });
+    }
+
+    callServiceGraphQL(`
+      query {
+        devices {
+          name
+        }
+      }
+    `)
+      .then(res => res.data.devices)
+      .then(devices => devices.map(device => device.name))
+      .then(deviceNames => this.setState({ deviceNames }));
   }
 
   toggleMode() {
@@ -247,6 +179,7 @@ class Dashboard extends Component {
               <Inspector
                 widget={this.state.widgets[this.state.selectedWidgetIndex]}
                 widgetDefinitions={WIDGET_DEFINITIONS}
+                deviceNames={this.state.deviceNames}
                 onParamChange={(param, value) =>
                   this.handleParamChange(param, value)
                 }
