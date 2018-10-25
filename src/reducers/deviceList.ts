b2da2c6e11dd9e9837ebde408628b375e1208859
Mikel Eguiraun <mikel.eguiraun@maxiv.lu.se>
2018-10-25 16:39:36 -0400

    sorting dev names ignoring cap cases

--- src/reducers/deviceList.ts
+++ src/reducers/deviceList.ts
@@ -35,7 +35,10 @@ export default function deviceList(state: IDeviceListState = {
 }, action) {
     switch (action.type) {
     case FETCH_DEVICE_NAMES_SUCCESS:
-        return {...state, nameList: action.names};
+        const sortedDeviceNames = action.names.sort((a, b) => {
+          return a.toLowerCase().localeCompare(b.toLowerCase());
+        });
+        return {...state, nameList: sortedDeviceNames};
     
     case SET_SEARCH_FILTER:
         return {...state, filter: action.filter};  
