c42420125791a051dd4a7657d4e156d1f7c3d8c4
hectorstudio <david.liu987@hotmail.com>
2018-10-25 15:56:07 -0400

    Added propType check for time prop

--- src/propTypes/propTypes.js
+++ src/propTypes/propTypes.js
@@ -1,42 +1,54 @@
-import { shape, number, string, oneOf, arrayOf, func, object } from 'prop-types';
+import {
+  shape,
+  number,
+  string,
+  oneOf,
+  arrayOf,
+  func,
+  object,
+  instanceOf
+} from "prop-types";
 
 export const widget = shape({
-	attribute: string,
-	device: string,
-    params: object,
-    type: string,
-	x: number,
-	y: number,
-})
+  attribute: string,
+  device: string,
+  params: object,
+  type: string,
+  date: instanceOf(Date),
+  x: number,
+  y: number
+});
 
 export const widgetDefinition = shape({
-    component: func,
-    fields: arrayOf(string),
-    name: string,
-    params: arrayOf(object),
-    type: string,
-})
+  component: func,
+  fields: arrayOf(string),
+  name: string,
+  params: arrayOf(object),
+  type: string
+});
 
 export const libraryWidgetDefinition = shape({
-    component: func,
-    field: arrayOf(string),
-    name: string,
-    params: arrayOf(object),
-    type: string,
-})
+  component: func,
+  field: arrayOf(string),
+  name: string,
+  params: arrayOf(object),
+  type: string
+});
 
 export const subCanvas = shape({
-    id: number,
-    name: string,
-    widgets: arrayOf(widget),
-})
+  id: number,
+  name: string,
+  widgets: arrayOf(widget)
+});
 
-export const command = arrayOf(shape({
+export const command = arrayOf(
+  shape({
     displevel: string,
     inttype: string,
     intypedesc: string,
     name: string,
     outtype: string,
     outtypedesc: string,
-    tag: number,
-  }))
+    tag: number
+  })
+);
