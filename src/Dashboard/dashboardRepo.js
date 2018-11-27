fabbd0837ce897d5ddbf6750c35a98b8327f516d
Jonas Rosenqvist <jrosenqvist@w-jonros-pc-0.maxiv.lu.se>
2018-11-27 11:40:36 +0100

    state and id

--- src/Dashboard/dashboardRepo.js
+++ src/Dashboard/dashboardRepo.js
@@ -1,20 +1,22 @@
-export const save = (state, callback) => {
+export const save = (id, canvases, callback) => {
   fetch('/dashboards/', {
     method: 'POST',
     headers: {
       "Content-Type": "application/json; charset=utf-8"
     },
-    body: JSON.stringify(state)
+    body: JSON.stringify({id : id, canvases: canvases})
     
   }).then((res) => res.json())
-  .then((res) => callback(res))
+  .then((res) => {
+    callback(res)
+  })
   .catch(function(){
     console.log("Couldn't reach dashboard repo");
   });
 }
 
-export const load = (callback) => {
-  fetch('/dashboards/', {
+export const load = (id, callback) => {
+  fetch('/dashboards/' + id, {
     method: 'GET',
     headers: {
       "Content-Type": "application/json; charset=utf-8"
