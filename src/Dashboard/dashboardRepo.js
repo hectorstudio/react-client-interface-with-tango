3befa48c01108d784d30fedc9f633c490cee429c
hectorstudio <david.liu987@hotmail.com>
2018-11-28 10:38:38 +0100

    prettier

--- src/Dashboard/dashboardRepo.js
+++ src/Dashboard/dashboardRepo.js
@@ -1,18 +1,18 @@
 export const save = (id, canvases) => {
-  return fetch('/dashboards/', {
-    method: 'POST',
+  return fetch("/dashboards/", {
+    method: "POST",
     headers: {
       "Content-Type": "application/json; charset=utf-8"
     },
-    body: JSON.stringify({id : id, canvases: canvases})
+    body: JSON.stringify({ id: id, canvases: canvases })
   });
-}
+};
 
-export const load = (id) => {
-  return fetch('/dashboards/' + id, {
-    method: 'GET',
+export const load = id => {
+  return fetch("/dashboards/" + id, {
+    method: "GET",
     headers: {
       "Content-Type": "application/json; charset=utf-8"
     }
-  })
-}
+  });
+};
