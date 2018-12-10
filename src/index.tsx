9526c4d438eef4b03016d4d530b529aa018c393e
Fredrik Bolmsten <fredrik.bolmsten@maxlab.lu.se>
2018-12-10 16:12:04 +0100

    new path for infopage

--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,18 @@ import * as React from 'react';
 import { render } from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import App from './App';
+import InfoPage from "./InfoPage/InfoPage";
+
 import './index.css';
 import { Route, BrowserRouter } from "react-router-dom";
 
 
 render(
   <BrowserRouter>
-    <Route path={process.env.REACT_APP_BASE_URL + ":tangoDB/"} component={App} />
+    <div>
+      <Route path={process.env.REACT_APP_BASE_URL + ":tangoDB/"} component={App} />
+      <Route path="/" exact={true} component={InfoPage}  />
+    </div>
   </BrowserRouter>,
   document.getElementById('root')
 );
