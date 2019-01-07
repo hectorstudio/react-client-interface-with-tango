cbc3305a9676ad50f01dd47b95b38dd15d45521c
Hannes Petri <hannes.petri@maxiv.lu.se>
2019-01-07 10:46:44 +0100

    Moved tango command execution to saga

--- src/reducers/commandOutput.ts
+++ src/reducers/commandOutput.ts
@@ -1,5 +1,5 @@
 import {
-    EXECUTE_COMMAND_COMPLETE
+    EXECUTE_COMMAND_SUCCESS
 } from '../actions/actionTypes';
 
 export interface ICommandOutputState {
@@ -10,7 +10,7 @@ export interface ICommandOutputState {
 
 export default function commandOutput(state: ICommandOutputState = {}, action) {
     switch (action.type) {
-    case EXECUTE_COMMAND_COMPLETE: {
+    case EXECUTE_COMMAND_SUCCESS: {
         const {command, result, device} = action;
         const forDevice = {...state[device], [command]: result};
         return {...state, [device]: forDevice};
