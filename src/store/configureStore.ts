c39033d78fd0eb14f31cbd3e87833ef2116e5f5f
hectorstudio <david.liu987@hotmail.com>
2019-01-08 12:50:45 +0100

    Minor cleanup in configureStore

--- src/store/configureStore.ts
+++ src/store/configureStore.ts
@@ -6,21 +6,25 @@ import rootReducer from "../reducers/rootReducer";
 import rootSaga from "../actions/sagas";
 import { LOGIN } from "../actions/actionTypes";
 
-export default function configureStore() {
-  const { __REDUX_DEVTOOLS_EXTENSION__ } = window as any;
-
-  const sagaMiddleware = createSagaMiddleware();
-  const loggerMiddleware = createLogger({
+function createLoggerMiddleware() {
+  return createLogger({
     actionTransformer: action =>
       action.type === LOGIN
         ? { ...action, password: action.password.replace(/./g, "*") }
         : action
   });
+}
+
+export default function configureStore() {
+  const { __REDUX_DEVTOOLS_EXTENSION__ } = window as any;
+
+  const sagaMiddleware = createSagaMiddleware();
+  const loggerMiddlware = createLoggerMiddleware();
 
   const store = createStore(
     rootReducer,
     __REDUX_DEVTOOLS_EXTENSION__ && __REDUX_DEVTOOLS_EXTENSION__(),
-    applyMiddleware(loggerMiddleware, sagaMiddleware)
+    applyMiddleware(loggerMiddlware, sagaMiddleware)
   );
 
   sagaMiddleware.run(rootSaga);
