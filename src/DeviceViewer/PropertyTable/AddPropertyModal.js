4bd52c95c38f3d1fd8401aba82873b03c435b609
hectorstudio <david.liu987@hotmail.com>
2019-01-10 12:50:51 +0100

    Improved form behaviour in modals

--- src/DeviceViewer/PropertyTable/AddPropertyModal.js
+++ src/DeviceViewer/PropertyTable/AddPropertyModal.js
@@ -1,6 +1,4 @@
 import React, { Component } from "react";
-import { Button } from "react-bootstrap";
-
 import Modal from "../../Modal/Modal";
 
 export default class AddPropertyModal extends Component {
@@ -15,50 +13,54 @@ export default class AddPropertyModal extends Component {
   render() {
     return (
       <Modal title={"Add Property"}>
-        <Modal.Body>
-          <div className="input-group mb-3">
-            <div className="input-group-prepend">
-              <span className="input-group-text">Name</span>
+        <form>
+          <Modal.Body>
+            <div className="input-group mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text">Name</span>
+              </div>
+              <input
+                type="text"
+                name="name"
+                className="form-control"
+                autoComplete="off"
+                value={this.state.name}
+                onChange={this.handleChangeName}
+              />
             </div>
-            <input
-              type="text"
-              name="name"
-              className="form-control"
-              autoComplete="off"
-              value={this.state.name}
-              onChange={this.handleChangeName}
-            />
-          </div>
 
-          <div className="input-group mb-3">
-            <div className="input-group-prepend">
-              <span className="input-group-text">Value</span>
+            <div className="input-group mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text">Value</span>
+              </div>
+              <input
+                type="text"
+                name="value"
+                className="form-control"
+                value={this.state.value}
+                onChange={this.handleChangeValue}
+              />
             </div>
-            <input
-              type="text"
-              name="value"
-              className="form-control"
-              value={this.state.value}
-              onChange={this.handleChangeValue}
-            />
-          </div>
-        </Modal.Body>
+          </Modal.Body>
 
-        <Modal.Footer>
-          <Button
-            className="btn btn-outline-secondary"
-            onClick={this.handleAdd}
-            disabled={!this.state.valid}
-          >
-            Create
-          </Button>
-          <Button
-            className="btn btn-outline-secondary"
-            onClick={() => this.props.onClose()}
-          >
-            Cancel
-          </Button>
-        </Modal.Footer>
+          <Modal.Footer>
+            <button
+              type="button"
+              className="btn btn-outline-secondary"
+              onClick={() => this.props.onClose()}
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="btn btn-primary"
+              onClick={this.handleAdd}
+              disabled={!this.state.valid}
+            >
+              Add
+            </button>
+          </Modal.Footer>
+        </form>
       </Modal>
     );
   }
@@ -74,7 +76,8 @@ export default class AddPropertyModal extends Component {
     this.setState({ value });
   }
 
-  handleAdd() {
+  handleAdd(event) {
+    event.preventDefault();
     const { name, value } = this.state;
     this.props.onAdd(name, value);
   }
