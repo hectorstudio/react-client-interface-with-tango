d59bfbd3c700a45b9c26ef8e91fe070d0bc31cd7
Hannes Petri <hannes.petri@maxiv.lu.se>
2019-01-11 13:08:07 +0100

    Made device selector order properties alphabetically

--- src/selectors/devices.ts
+++ src/selectors/devices.ts
@@ -1,4 +1,5 @@
 import { createSelector } from "reselect";
+
 import { IRootState } from "../reducers/rootReducer";
 import { objectValues } from "../utils";
 
@@ -10,20 +11,41 @@ function getDevices(state: IRootState) {
   return state.devices;
 }
 
-export const getDevice = (name: string) => createSelector(
-  getDevices,
-  getPropertiesState,
-  getAttributesState,
-  getCommandsState,
-  (devices, propertiesState, attributesState, commandsState) => {
-    const device = devices[name];
-    if (device == null) {
-      return null;
-    }
+interface IHasName {
+  name: string;
+}
 
-    const properties = objectValues(propertiesState[name]);
-    const attributes = objectValues(attributesState[name]);
-    const commands = objectValues(commandsState[name]);
-    return { ...device, properties, attributes, commands };
+function compareByName(property1: IHasName, property2: IHasName) {
+  const name1 = property1.name.toUpperCase();
+  const name2 = property2.name.toUpperCase();
+  if (name1 < name2) {
+    return -1;
+  } else if (name1 > name2) {
+    return 1;
+  } else {
+    return 0;
   }
-);
+}
+
+function sortedByName(entries: IHasName[]) {
+  return entries.concat().sort(compareByName);
+}
+
+export const getDevice = (name: string) =>
+  createSelector(
+    getDevices,
+    getPropertiesState,
+    getAttributesState,
+    getCommandsState,
+    (devices, propertiesState, attributesState, commandsState) => {
+      const device = devices[name];
+      if (device == null) {
+        return null;
+      }
+
+      const properties = sortedByName(objectValues(propertiesState[name]));
+      const attributes = objectValues(attributesState[name]);
+      const commands = objectValues(commandsState[name]);
+      return { ...device, properties, attributes, commands };
+    }
+  );
