8da2f47261126cd8432bf24dfe9a326c884a5742
Hannes Petri <hannes.petri@maxiv.lu.se>
2019-01-11 09:35:12 +0100

    Changed working and style in delete property modal

--- src/DeviceViewer/PropertyTable/DeletePropertyModal.js
+++ src/DeviceViewer/PropertyTable/DeletePropertyModal.js
@@ -4,20 +4,20 @@ import { Button } from "react-bootstrap";
 import Modal from "../../Modal/Modal";
 
 const DeletePropertyModal = ({ name, onDelete, onClose }) => (
-  <Modal title={"Remove Property"}>
+  <Modal>
     <Modal.Body>
       <p>
-        Are you sure you want to remove property{" "}
+        Are you sure you want to delete property{" "}
         <span style={{ fontWeight: "bold" }}>{name}</span>?
       </p>
     </Modal.Body>
 
     <Modal.Footer>
-      <Button className="btn btn-outline-secondary" onClick={() => onDelete(name)}>
-        Yes
-      </Button>
       <Button className="btn btn-outline-secondary" onClick={onClose}>
-        No
+        Cancel
+      </Button>
+      <Button className="btn btn-primary" onClick={() => onDelete(name)}>
+        Delete
       </Button>
     </Modal.Footer>
   </Modal>
