b7e5e21904b91d8159c5e7b947a68cfa57749a17
hectorstudio <david.liu987@hotmail.com>
2019-01-15 16:59:33 +0100

    Temporarily disabled dashboard repo communication

--- src/dashboard/dashboardRepo.js
+++ src/dashboard/dashboardRepo.js
@@ -1,18 +1,22 @@
-export const save = (id, canvases) => {
-  return fetch("/dashboards/", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json; charset=utf-8"
-    },
-    body: JSON.stringify({ id: id, canvases: canvases })
-  }).then(res => (res.ok ? res.json() : null));
-};
+// export const save = (id, canvases) => {
+//   return fetch("/dashboards/", {
+//     method: "POST",
+//     headers: {
+//       "Content-Type": "application/json; charset=utf-8"
+//     },
+//     body: JSON.stringify({ id: id, canvases: canvases })
+//   }).then(res => (res.ok ? res.json() : null));
+// };
 
-export const load = id => {
-  return fetch("/dashboards/" + id, {
-    method: "GET",
-    headers: {
-      "Content-Type": "application/json; charset=utf-8"
-    }
-  }).then(res => res.json());
-}; 
+// export const load = id => {
+//   return fetch("/dashboards/" + id, {
+//     method: "GET",
+//     headers: {
+//       "Content-Type": "application/json; charset=utf-8"
+//     }
+//   }).then(res => res.json());
+// }; 
+
+
+export const save = (id, canvases) => new Promise((res, rej) => res());
+export const load = (id, canvases) => new Promise((res, rej) => res());
