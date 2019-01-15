c91f11f8722705bf190729c12504656c9ee20330
hectorstudio <david.liu987@hotmail.com>
2019-01-15 17:22:30 +0100

    Changed jive to use saga creator

--- src/jive/actions/sagas/index.js
+++ src/jive/actions/sagas/index.js
@@ -1,9 +1,9 @@
 import { fork } from "redux-saga/effects";
 
-import user from "../../../shared/user/state/saga";
+import createUserSaga from "../../../shared/user/state/saga";
 import tango from "./tango";
 
 export default function* sagas() {
-  yield fork(user);
+  yield fork(createUserSaga());
   yield fork(tango);
 }
