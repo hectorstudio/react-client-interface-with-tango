b184b7500d0993cfcdb32fde900027715c07e51b
Hannes Petri <hannes.petri@maxiv.lu.se>
2019-01-18 15:13:16 +0100

    Added AttributeSelect component

---
+++ src/dashboard/components/Inspector/AttributeSelect.tsx
@@ -0,0 +1,110 @@
+import React, { Component, FormEvent } from "react";
+
+import { fetchAttributes, fetchDeviceNames } from "./api";
+
+interface IProps {
+  device?: string;
+  attribute?: string;
+  onSelect?: (device: string | null, attribute: string | null) => void;
+}
+
+interface IState {
+  fetchingAttributes: boolean;
+  deviceNames: string[];
+  attributes: Array<{
+    name: string;
+    datatype: string;
+    dataformat: string;
+  }>;
+}
+
+export default class AttributeSelect extends Component<IProps, IState> {
+  public constructor(props) {
+    super(props);
+    this.state = { fetchingAttributes: false, deviceNames: [], attributes: [] };
+    this.handleSelectAttribute = this.handleSelectAttribute.bind(this);
+  }
+
+  public async componentDidMount() {
+    // Eventually shouldn't be done by this component
+    const deviceNames = await fetchDeviceNames("kitslab");
+    this.setState({ deviceNames });
+
+    this.fetchAttributes();
+  }
+
+  public componentDidUpdate(prevProps) {
+    if (this.props.device !== prevProps.device) {
+      this.fetchAttributes();
+    }
+  }
+
+  public handleSelectDevice(event: FormEvent<HTMLSelectElement>) {
+    this.fetchAttributes();
+    const { onSelect } = this.props;
+    const newDevice = event.currentTarget.value;
+    if (onSelect && newDevice) {
+      onSelect(newDevice, null);
+    }
+  }
+
+  public handleSelectAttribute(event: FormEvent<HTMLSelectElement>) {
+    const { onSelect, device } = this.props;
+    const newAttribute = event.currentTarget.value;
+    if (onSelect && device && newAttribute) {
+      onSelect(device, newAttribute);
+    }
+  }
+
+  public render() {
+    const { device, attribute } = this.props;
+
+    return (
+      <div>
+        <select>
+          {device == null && <option>Select device</option>}
+          {this.state.deviceNames.map(name => (
+            <option>{name}</option>
+          ))}
+        </select>
+        <br />
+        <select disabled={false} onChange={this.handleSelectAttribute}>
+          {attribute == null && (
+            <option
+              selected={true}
+              disabled={this.state.fetchingAttributes || device == null}
+            >
+              Select attribute
+            </option>
+          )}
+          {this.state.attributes.map(attr => (
+            <option>{attr.name}</option>
+          ))}
+        </select>
+      </div>
+    );
+  }
+
+  // private filteredAttributes() {
+  //   const numericTypes = [
+  //     "DevDouble",
+  //     "DevFloat",
+  //     "DevLong",
+  //     "DevLong64",
+  //     "DevShort",
+  //     "DevUChar",
+  //     "DevULong",
+  //     "DevULong64",
+  //     "DevUShort"
+  //   ];
+  // }
+
+  private async fetchAttributes() {
+    const { device } = this.props;
+    if (device) {
+      this.setState({ fetchingAttributes: true });
+      const attributes = await fetchAttributes("kitslab", device);
+      this.setState({ attributes, fetchingAttributes: false });
+    }
+  }
+}
