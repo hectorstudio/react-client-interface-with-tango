c215d52d10d69a638ec72cdb49469928570816d6
hectorstudio <david.liu987@hotmail.com>
2019-01-21 14:46:41 +0100

    Reduxified widget movement

--- src/dashboard/state/actionTypes.js
+++ src/dashboard/state/actionTypes.js
@@ -1,4 +1,6 @@
 export const SELECT_WIDGET = "SELECT_WIDGET";
 export const DELETE_WIDGET = "DELETE_WIDGET";
 export const ADD_WIDGET = "ADD_WIDGET";
+export const MOVE_WIDGET = "MOVE_WIDGET";
+
 export const SELECT_CANVAS = "SELECT_CANVAS";
