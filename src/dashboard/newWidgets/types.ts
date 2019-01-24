bf64be4b0572baba0751640d698e2429867c3dfb
Hannes Petri <hannes.petri@maxiv.lu.se>
2019-01-24 13:50:32 +0100

    Passing actual width and height to widget no enable absolute sizing when needed by a library

--- src/dashboard/newWidgets/types.ts
+++ src/dashboard/newWidgets/types.ts
@@ -3,4 +3,6 @@ import { IInputMapping } from "../types";
 export interface IWidgetProps {
   inputs: IInputMapping;
   mode: "run" | "edit" | "library";
+  actualWidth: number;
+  actualHeight: number;
 }
