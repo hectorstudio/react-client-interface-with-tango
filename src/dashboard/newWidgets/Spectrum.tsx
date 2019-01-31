f65727206e23768962f8fc4042dd053011c7cb8e
hectorstudio <david.liu987@hotmail.com>
2019-01-31 16:57:09 +0100

    Inelastic y axis

--- src/dashboard/newWidgets/Spectrum.tsx
+++ src/dashboard/newWidgets/Spectrum.tsx
@@ -7,10 +7,38 @@ import { IWidgetDefinition } from "../types";
 // prettier-ignore
 const sampleData = [0, -2, 3, -2, 1, -5, 4, -3, -2, -4, 0, -4, 2, 2, -2, -2, 2, -5, -2, -3, 0];
 
-class Spectrum extends Component<IWidgetProps> {
+interface IState {
+  min?: number;
+  max?: number;
+}
+
+class Spectrum extends Component<IWidgetProps, IState> {
+  public constructor(props: IWidgetProps) {
+    super(props);
+    this.state = {};
+  }
+
+  public componentDidUpdate() {
+    if (this.props.mode !== "run") {
+      return;
+    }
+
+    const { value } = this.props.inputs.attribute;
+    const { min: currMin, max: currMax } = this.state;
+
+    const propMin = value.reduce((a, b) => Math.min(a, b));
+    const propMax = value.reduce((a, b) => Math.max(a, b));
+    const min = currMin == null ? propMin : Math.min(currMin, propMin);
+    const max = currMax == null ? propMax : Math.max(currMax, propMax);
+
+    if (min !== currMin || max !== currMax) {
+      this.setState({ min, max });
+    }
+  }
+
   public render() {
     const { mode, inputs } = this.props;
-    const { attribute, showTitle } = inputs;
+    const { attribute, showTitle, inelastic } = inputs;
     const title =
       showTitle === false
         ? null
@@ -21,6 +49,7 @@ class Spectrum extends Component<IWidgetProps> {
     const y =
       mode === "run" ? attribute.value : mode === "library" ? sampleData : [];
     const data = [{ y }];
+    const yaxis = this.yAxis(inelastic);
 
     const layout = {
       title,
@@ -33,17 +62,31 @@ class Spectrum extends Component<IWidgetProps> {
         b: 20
       },
       width: this.props.actualWidth,
-      height: mode !== "library" ? this.props.actualHeight : 200
+      height: mode !== "library" ? this.props.actualHeight : 200,
+      yaxis
     };
 
     return (
-      <Plotly
-        data={data}
-        layout={layout}
-        config={{ staticPlot: true }}
-        responsive={true}
-      />
+      <div>
+        <Plotly
+          data={data}
+          layout={layout}
+          config={{ staticPlot: true }}
+          responsive={true}
+        />
+      </div>
     );
+  }
+
+  private yAxis(inelastic: boolean): { range: [number, number] } | {} {
+    const { min, max } = this.state;
+    if (inelastic && min != null && max != null) {
+      return {
+        range: [1.25 * min, 1.25 * max]
+      };
+    } else {
+      return {};
+    }
   }
 }
 
@@ -63,6 +106,11 @@ const definition: IWidgetDefinition = {
     showTitle: {
       type: "boolean",
       label: "Show Title",
+      default: true
+    },
+    inelastic: {
+      type: "boolean",
+      label: "Inelastic Y Axis",
       default: true
     }
   }
