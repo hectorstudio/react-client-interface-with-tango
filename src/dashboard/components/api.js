402e0eab300f6bd3bfe56a954298c10d9f6c2a28
Hannes Petri <hannes.petri@maxiv.lu.se>
2019-02-01 14:19:39 +0100

    Added elements to deal with command inputs

--- src/dashboard/components/api.js
+++ src/dashboard/components/api.js
@@ -1,12 +1,22 @@
 import createGQLClient from "graphql-client";
 
 const FETCH_ATTRIBUTES = `
-query FetchNames($device: String!) {
+query FetchAttributeNames($device: String!) {
   device(name: $device) {
     attributes {
       name
       dataformat
       datatype
+    }
+  }
+}
+`;
+
+const FETCH_COMMANDS = `
+query FetchCommandNames($device: String!) {
+  device(name: $device) {
+    commands {
+      name
     }
   }
 }
@@ -28,6 +38,15 @@ export async function fetchAttributes(tangoDB, device) {
   try {
     const res = await createClient(tangoDB).query(FETCH_ATTRIBUTES, { device });
     return res.data.device.attributes;
+  } catch (err) {
+    return [];
+  }
+}
+
+export async function fetchCommands(tangoDB, device) {
+  try {
+    const res = await createClient(tangoDB).query(FETCH_COMMANDS, { device });
+    return res.data.device.commands;
   } catch (err) {
     return [];
   }
