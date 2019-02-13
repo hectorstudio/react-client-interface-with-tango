aac3d8095d1b419250e6d3de8de4d134c68f163f
Hannes Petri <hannes.petri@maxiv.lu.se>
2019-02-13 15:37:44 +0100

    Added cooldown to command executor

--- src/dashboard/widgets/CommandExecutor.tsx
+++ src/dashboard/widgets/CommandExecutor.tsx
@@ -3,19 +3,36 @@ import React, { Component, CSSProperties } from "react";
 import { WidgetProps } from "./types";
 import { WidgetDefinition, CommandInput } from "../types";
 
+function timeout(seconds) {
+  return new Promise(resolve => {
+    setTimeout(() => resolve(), seconds);
+  });
+}
+
 interface Inputs {
   title: string;
   requireConfirmation: string;
   command: CommandInput;
   displayOutput: boolean;
+  cooldown: number;
 }
 
 type Props = WidgetProps<Inputs>;
 
-class CommandExecutor extends Component<Props> {
+interface State {
+  cooldown: boolean;
+}
+
+class CommandExecutor extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { cooldown: false };
+    this.handleExecute = this.handleExecute.bind(this);
+  }
+
   public render() {
     const { mode, inputs } = this.props;
-    const { title, requireConfirmation, command } = inputs;
+    const { title, command } = inputs;
     const actualTitle = title || command.command || "command";
 
     const style: CSSProperties =
@@ -31,21 +48,33 @@ class CommandExecutor extends Component<Props> {
     return (
       <div style={containerStyle}>
         <button
+          disabled={this.state.cooldown}
           style={style}
-          onClick={() => {
-            const message = `Confirm executing "${command.command}" on "${
-              command.device
-            }"`;
-            if (!requireConfirmation || confirm(message)) {
-              command.execute();
-            }
-          }}
+          onClick={this.handleExecute}
         >
           {actualTitle}
         </button>
         <span style={fullOutputStyle}>{output}</span>
       </div>
     );
+  }
+
+  private async handleExecute() {
+    const { command, requireConfirmation, cooldown } = this.props.inputs;
+
+    const message = `Confirm executing "${command.command}" on "${
+      command.device
+    }"`;
+
+    if (!requireConfirmation || confirm(message)) {
+      command.execute();
+
+      if (cooldown > 0) {
+        this.setState({ cooldown: true });
+        await timeout(1000 * cooldown);
+        this.setState({ cooldown: false });
+      }
+    }
   }
 
   private outputAndStyle(): [string, CSSProperties] {
@@ -96,6 +125,12 @@ const definition: WidgetDefinition = {
       type: "boolean",
       label: "Display Output",
       default: true
+    },
+    cooldown: {
+      type: "number",
+      label: "Cooldown (s)",
+      default: 0,
+      nonNegative: true
     }
   }
 };
