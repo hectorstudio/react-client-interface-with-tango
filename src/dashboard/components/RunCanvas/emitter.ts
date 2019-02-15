1882137056234f52fd79cba9613970b8160049d4
Hannes Petri <hannes.petri@maxiv.lu.se>
2019-02-15 12:35:22 +0100

    Closed connection management and working attribute writing from widgets

--- src/dashboard/components/RunCanvas/emitter.ts
+++ src/dashboard/components/RunCanvas/emitter.ts
@@ -7,18 +7,33 @@ subscription Attributes($fullNames: [String]!) {
   }
 }`;
 
-function socketUrl(tangoDB) {
+export const END = Symbol("END");
+
+interface Frame {
+  device: string;
+  attribute: string;
+  value: any;
+}
+
+type EmitHandler = (frame: Frame | typeof END) => void;
+type Unsub = () => void;
+type EmissionStarter = (emit: EmitHandler) => Unsub;
+
+function socketUrl(tangoDB: string) {
   const loc = window.location;
   const protocol = loc.protocol.replace("http", "ws");
   return protocol + "//" + loc.host + "/" + tangoDB + "/socket?dashboard";
 }
 
-function createSocket(tangoDB) {
+function createSocket(tangoDB: string) {
   return new WebSocket(socketUrl(tangoDB), "graphql-ws");
 }
 
-export function changeEventEmitter(tangoDB, fullNames) {
-  return emit => {
+export function changeEventEmitter(
+  tangoDB: string,
+  fullNames: string[]
+): EmissionStarter {
+  return emitHandler => {
     const socket = createSocket(tangoDB);
     socket.addEventListener("open", () => {
       const variables = { fullNames };
@@ -36,8 +51,12 @@ export function changeEventEmitter(tangoDB, fullNames) {
     socket.addEventListener("message", msg => {
       const frame = JSON.parse(msg.data);
       if (frame.type === "data" && frame.payload.error == null) {
-        emit(frame.payload.data.attributes);
+        emitHandler(frame.payload.data.attributes as Frame);
       }
+    });
+
+    socket.addEventListener("close", () => {
+      emitHandler(END);
     });
 
     return () => {
