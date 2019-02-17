59b7c9e45b743660d7561bc4eea4547c113a3ca6
hectorstudio <david.liu987@hotmail.com>
2019-02-17 22:57:41 +0100

    Beautified multiple selection placeholder in sidebar

--- src/dashboard/components/Sidebar.tsx
+++ src/dashboard/components/Sidebar.tsx
@@ -4,6 +4,26 @@ import Library from "./Library/Library";
 
 import { Widget } from "../types";
 
+const MultipleSelection = () => (
+  <div
+    style={{
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      height: "100%"
+    }}
+  >
+    <span
+      style={{
+        fontWeight: "bold",
+        color: "lightgray"
+      }}
+    >
+      Multiple selection
+    </span>
+  </div>
+);
+
 interface Props {
   mode: "run" | "edit";
   tangoDB: string;
@@ -29,7 +49,7 @@ export default class Sidebar extends Component<Props> {
             tangoDB={tangoDB}
           />
         ) : (
-          "Multiple selection"
+          <MultipleSelection />
         )}
       </div>
     );
