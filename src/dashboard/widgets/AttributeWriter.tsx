d60c6f32e2574bdef4c2dab1027881d0e943ccf0
Hannes Petri <hannes.petri@maxiv.lu.se>
2019-02-21 14:28:55 +0100

    Implemented attribute writer (for numeric values)

---
+++ src/dashboard/widgets/AttributeWriter.tsx
@@ -0,0 +1,132 @@
+import React, { Component, FormEvent } from "react";
+
+import { WidgetProps } from "./types";
+import { WidgetDefinition, AttributeInput } from "../types";
+
+interface Inputs {
+  attribute: AttributeInput;
+  showDevice: boolean;
+  showAttribute: boolean;
+}
+
+type Props = WidgetProps<Inputs>;
+
+interface State {
+  input: string;
+  pending: boolean;
+}
+
+class AttributeWriter extends Component<Props, State> {
+  public constructor(props: Props) {
+    super(props);
+    this.state = {
+      input: "",
+      pending: false
+    };
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  public render() {
+    const { inputs } = this.props;
+    const { attribute, showDevice, showAttribute } = inputs;
+
+    const deviceLabel = attribute.device || "device";
+    const attributeLabel = attribute.attribute || "attribute";
+
+    const label = [
+      ...(showDevice ? [deviceLabel] : []),
+      ...(showAttribute ? [attributeLabel] : [])
+    ].join("/");
+
+    const dataType = this.dataType();
+    const input =
+      dataType === "boolean" ? (
+        <span style={{ backgroundColor: "red" }}>
+          DevBoolan not implemented
+        </span>
+      ) : (
+        <input
+          type="text"
+          style={{
+            flexGrow: 1,
+            minWidth: "3em"
+          }}
+          value={this.state.input}
+          onChange={e => this.setState({ input: e.target.value })}
+        />
+      );
+
+    return (
+      <form
+        style={{
+          display: "flex",
+          alignItems: "center",
+          padding: "0.25em 0.5em"
+        }}
+        onSubmit={this.handleSubmit}
+      >
+        {label && (
+          <span style={{ flexGrow: 0, marginRight: "0.5em" }}>{label}:</span>
+        )}
+        {input}
+      </form>
+    );
+  }
+
+  private dataType(): "numeric" | "boolean" | "string" {
+    const { attribute } = this.props.inputs;
+    const { dataType, isNumeric } = attribute;
+    const isBoolean = dataType === "DevBoolean";
+    return isNumeric ? "numeric" : isBoolean ? "boolean" : "string";
+  }
+
+  private async handleSubmit(event: FormEvent<HTMLFormElement>) {
+    if (this.state.pending) {
+      return;
+    }
+
+    event.preventDefault();
+    const { attribute } = this.props.inputs;
+    const { isNumeric } = attribute;
+    // const isBoolean = attribute.dataType === "DevBoolean";
+    const { input } = this.state;
+    
+    const value = isNumeric ? Number(input) : input;
+    if (typeof value === "number" && isNaN(value)) {
+      return;
+    }
+
+    this.setState({ input: "", pending: true });
+    await this.props.inputs.attribute.write(value);
+    this.setState({ pending: false });
+  }
+}
+
+const definition: WidgetDefinition = {
+  type: "ATTRIBUTE_WRITER",
+  name: " Attribute Writer",
+  defaultHeight: 2,
+  defaultWidth: 15,
+  inputs: {
+    attribute: {
+      type: "attribute",
+      label: "",
+      dataFormat: "scalar"
+    },
+    showDevice: {
+      type: "boolean",
+      label: "Show Device",
+      default: true
+    },
+    showAttribute: {
+      type: "boolean",
+      label: "Show Attribute",
+      default: true
+    }
+  }
+};
+
+export default {
+  definition,
+  component: AttributeWriter
+};
