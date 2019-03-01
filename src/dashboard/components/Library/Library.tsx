e2ea1df0081de8b64b8cb51f653b438221c2b78b
hectorstudio <david.liu987@hotmail.com>
2019-03-01 10:40:49 +0100

    Cleaned up library component

--- src/dashboard/components/Library/Library.tsx
+++ src/dashboard/components/Library/Library.tsx
@@ -7,37 +7,10 @@ export default class Library extends Component {
   public render() {
     return (
       <div className="Library">
-        {/*<h1>Built-In</h1>*/}
         {bundles.map((bundle, i) => {
           return <LibraryWidget key={i} bundle={bundle} />;
         })}
       </div>
     );
-    /*
-    const builtIn = this.props.widgetDefinitions.filter(
-      definition => definition.__canvas__ == null
-    );
-
-    const custom = this.props.widgetDefinitions.filter(
-      definition => definition.__canvas__ != null
-    );
-
-    return (
-      <div className="Library">
-        <h1>Built-In</h1>
-        {builtIn.map((definition, i) => {
-          return <LibraryWidget key={i} definition={definition} />;
-        })}
-        {this.props.showCustom && (
-          <Fragment>
-            <h1>Custom</h1>
-            {custom.map((definition, i) => {
-              return <LibraryWidget key={i} definition={definition} />;
-            })}
-          </Fragment>
-        )}
-      </div>
-    );
-    */
   }
 }
