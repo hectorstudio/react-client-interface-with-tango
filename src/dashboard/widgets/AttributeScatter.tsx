32eacaa5d32281a21db9ce21ccf2394be7bde144
hectorstudio <david.liu987@hotmail.com>
2019-03-14 11:04:30 +0100

    Changed scatter symbol to cross; added sample data for the library mode

--- src/dashboard/widgets/AttributeScatter.tsx
+++ src/dashboard/widgets/AttributeScatter.tsx
@@ -64,6 +64,11 @@ function fullName(attribute: AttributeInput) {
   return `${attribute.device || "?"}/${attribute.attribute || "?"}`;
 }
 
+// prettier-ignore
+const sampleX = [1.04, 2.4, 2.11, 2.72, 3.81, 3.21, 4.81, 5.76, 4.63, 6.66, 5.83, 7.33, 8.41, 8.91, 9.44, 9.73, 10.15, 9.72];
+// prettier-ignore
+const sampleY = [1.41, 1.16, 2.36, 2.72, 2.62, 3.96, 4.01, 4.13, 5.07, 5.15, 6.29, 6.61, 7.35, 7.83, 7.59, 8.11, 9.44, 9.79];
+
 function AttributeScatter(props: Props) {
   const { mode, inputs, actualWidth, actualHeight } = props;
   const staticMode = mode !== "run";
@@ -75,7 +80,7 @@ function AttributeScatter(props: Props) {
   const dependentName =
     mode === "library" ? "attribute 2" : fullName(dependent);
 
-  const defaultRange = mode !== "run" ? { range: [0, 100] } : {};
+  const defaultRange = mode !== "run" ? { range: [0, 10] } : {};
 
   const layout = {
     font: { family: "Helvetica, Arial, sans-serif" },
@@ -100,23 +105,32 @@ function AttributeScatter(props: Props) {
     }
   };
 
-  const dependentHistory = dependent.history || [];
-  const independentHistory = independent.history || [];
+  let x: number[] = [];
+  let y: number[] = [];
 
-  const fun = interpolated(
-    dependentHistory.map(attr => attr.timestamp),
-    dependentHistory.map(attr => attr.value)
-  );
+  if (mode === "run") {
+    const dependentHistory = dependent.history || [];
+    const independentHistory = independent.history || [];
 
-  const x = independentHistory.map(attr => attr.value);
-  const y = independentHistory.map(attr => fun(attr.timestamp));
+    const fun = interpolated(
+      dependentHistory.map(attr => attr.timestamp),
+      dependentHistory.map(attr => attr.value)
+    );
+
+    x = independentHistory.map(attr => attr.value);
+    y = independentHistory.map(attr => fun(attr.timestamp));
+  } else if (mode === "library") {
+    x = sampleX;
+    y = sampleY;
+  }
 
   const data = [
     {
       type: "scatter",
       mode: "markers",
       x,
-      y
+      y,
+      marker: { symbol: "cross" }
     }
   ];
 
