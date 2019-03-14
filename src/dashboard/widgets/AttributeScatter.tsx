bfeb818e8a553bce11c725adc5b8bb81ae30faa6
Hannes Petri <hannes.petri@maxiv.lu.se>
2019-03-14 13:52:24 +0100

    Fixed error in interpolation function

--- src/dashboard/widgets/AttributeScatter.tsx
+++ src/dashboard/widgets/AttributeScatter.tsx
@@ -36,6 +36,14 @@ type Props = WidgetProps<Inputs>;
 
 // Naive lerp implementation, with fallback to first/last value of unknown is ouside range
 function interpolated(xs: number[], ys: number[]) {
+  if (xs.length !== ys.length) {
+    throw new Error("xs.length != ys.length");
+  }
+
+  if (xs.length === 0) {
+    throw new Error("xs.length == 0");
+  }
+
   return (input: number) => {
     if (input < xs[0]) {
       return ys[0];
@@ -44,12 +52,12 @@ function interpolated(xs: number[], ys: number[]) {
     for (let i = 0; i < xs.length - 1; i++) {
       const currX = xs[i];
       const nextX = xs[i + 1];
-      const deltaX = nextX - currX;
 
-      if (input >= currX && input <= nextX) {
+      if (input >= currX && input < nextX) {
+        const deltaX = nextX - currX;
         if (deltaX !== 0) {
-          const w1 = (input - currX) / deltaX;
-          const w2 = (nextX - input) / deltaX;
+          const w1 = (nextX - input) / deltaX;
+          const w2 = (input - currX) / deltaX;
           return w1 * ys[i] + w2 * ys[i + 1];
         } else {
           return ys[i];
@@ -66,9 +74,9 @@ function fullName(attribute: AttributeInput) {
 }
 
 // prettier-ignore
-const sampleX = [1.04, 2.4, 2.11, 2.72, 3.81, 3.21, 4.81, 5.76, 4.63, 6.66, 5.83, 7.33, 8.41, 8.91, 9.44, 9.73, 10.15, 9.72];
+const sampleX = [-3.96, -2.6, -2.89, -2.28, -1.19, -1.79, -0.19, 0.76, -0.37, 1.66, 0.83, 2.33, 3.41, 3.91, 4.44, 4.73, 5.15, 4.72];
 // prettier-ignore
-const sampleY = [1.41, 1.16, 2.36, 2.72, 2.62, 3.96, 4.01, 4.13, 5.07, 5.15, 6.29, 6.61, 7.35, 7.83, 7.59, 8.11, 9.44, 9.79];
+const sampleY = [-3.59, -3.84, -2.64, -2.28, -2.38, -1.04, -0.99, -0.87, 0.07, 0.15, 1.29, 1.61, 2.35, 2.83, 2.59, 3.11, 4.44, 4.79];
 
 function AttributeScatter(props: Props) {
   const { mode, inputs, actualWidth, actualHeight } = props;
@@ -81,7 +89,7 @@ function AttributeScatter(props: Props) {
   const dependentName =
     mode === "library" ? "attribute 2" : fullName(dependent);
 
-  const defaultRange = mode !== "run" ? { range: [0, 10] } : {};
+  const defaultRange = mode !== "run" ? { range: [-5, 5] } : {};
 
   const layout = {
     font: { family: "Helvetica, Arial, sans-serif" },
@@ -110,16 +118,18 @@ function AttributeScatter(props: Props) {
   let y: number[] = [];
 
   if (mode === "run") {
-    const dependentHistory = dependent.history || [];
-    const independentHistory = independent.history || [];
+    const dependentHistory = dependent.history;
+    const independentHistory = independent.history;
 
-    const fun = interpolated(
-      dependentHistory.map(attr => attr.timestamp),
-      dependentHistory.map(attr => attr.value)
-    );
+    if (dependentHistory.length > 0 && independentHistory.length > 0) {
+      const fun = interpolated(
+        dependentHistory.map(attr => attr.timestamp),
+        dependentHistory.map(attr => attr.value)
+      );
 
-    x = independentHistory.map(attr => attr.value);
-    y = independentHistory.map(attr => fun(attr.timestamp));
+      x = independentHistory.map(attr => attr.value);
+      y = independentHistory.map(attr => fun(attr.timestamp));
+    }
   } else if (mode === "library") {
     x = sampleX;
     y = sampleY;
