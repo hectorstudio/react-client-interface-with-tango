6cb29ca97586cc2658a1bcfa9cde7ae277f99f5d
Hannes Petri <hannes.petri@maxiv.lu.se>
2019-04-12 14:10:32 +0200

    Resolved various warnings

--- src/jive/components/InfoPage/InfoPage.js
+++ src/jive/components/InfoPage/InfoPage.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './InfoPage.css'
 
-const InfoPage = ({}) =>{
+const InfoPage = () =>{
         return <p>You need to specify the beamline in the url for webjive to work, such as https://{window.location.hostname}/<b>beamline</b>/. It is important to have a / after the beamline for webjive to work.</p>;
 }
 export default InfoPage;
