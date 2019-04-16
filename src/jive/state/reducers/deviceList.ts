774a79ea9091ec13da8dfe5818ff9e9365dbcd19
hectorstudio <david.liu987@hotmail.com>
2019-04-16 16:19:49 +0200

    Improved typing in jive reducers

--- src/jive/state/reducers/deviceList.ts
+++ src/jive/state/reducers/deviceList.ts
@@ -1,28 +1,26 @@
-import {
-    FETCH_DEVICE_NAMES_SUCCESS,
-    SET_SEARCH_FILTER
-} from '../actions/actionTypes';
+import { FETCH_DEVICE_NAMES_SUCCESS } from "../actions/actionTypes";
+import JiveAction from "../actions";
 
 export interface IDeviceListState {
-    filter: string;
-    nameList: string[];
+  nameList: string[];
 }
 
-export default function deviceList(state: IDeviceListState = {
-    filter: '',
-    nameList: []
-}, action) {
-    switch (action.type) {
-    case FETCH_DEVICE_NAMES_SUCCESS:
-        const sortedDeviceNames = action.names.sort((a, b) => {
-          return a.toLowerCase().localeCompare(b.toLowerCase());
-        });
-        return {...state, nameList: sortedDeviceNames};
-    
-    case SET_SEARCH_FILTER:
-        return {...state, filter: action.filter};  
+const initialState = {
+  nameList: []
+};
 
-    default:
-        return state;
+export default function deviceList(
+  state: IDeviceListState = initialState,
+  action: JiveAction
+): IDeviceListState {
+  switch (action.type) {
+    case FETCH_DEVICE_NAMES_SUCCESS: {
+      const sortedDeviceNames = action.names.sort((a, b) => {
+        return a.toLowerCase().localeCompare(b.toLowerCase());
+      });
+      return { ...state, nameList: sortedDeviceNames };
     }
+    default:
+      return state;
+  }
 }
