a3f8b0823040ea6a0295a7101c9cc7e963d95260
hectorstudio <david.liu987@hotmail.com>
2019-07-26 12:04:08 +0100

    Initial commit of branch AT2-161 for Logger Widget

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,28 +1,29 @@
 const proxy = require("http-proxy-middleware");
 
 const proxies = {
-  "/kitslab/socket": {
-    target: "https://webjive-test.maxiv.lu.se",
+  "/testdb/socket": {
+    target: "http://localhost:22484",
     secure: false,
     changeOrigin: true,
     ws: true
   },
-  "/kitslab/db": {
-    target: "https://webjive-test.maxiv.lu.se",
+  "/testdb/db": {
+    target: "http://localhost:22484",
     secure: false,
     changeOrigin: true
   },
   "/auth": {
-    target: "https://webjive-test.maxiv.lu.se",
+    target: "http://localhost:22484",
     secure: false,
     changeOrigin: true
   },
   "/dashboards": {
-    target: "https://webjive-test.maxiv.lu.se",
+    target: "http://localhost:22484",
     secure: false,
     changeOrigin: true
   }
 };
+
 
 module.exports = function(app) {
   Object.entries(proxies).forEach(([endpoint, setting]) => {
