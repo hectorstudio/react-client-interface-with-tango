8977e0627711713dcf02d8fb2185e19188d4d391
hectorstudio <david.liu987@hotmail.com>
2019-07-28 23:34:19 +0200

    Added incomplete mapper from input definition to widget props

--- src/dashboard/widgets/types.ts
+++ src/dashboard/widgets/types.ts
@@ -1,7 +1,38 @@
-export interface WidgetProps<T = Record<string, any>> {
-  inputs: T;
-  mode: "run" | "edit" | "library";
+import {
+  NumberInputDefinition,
+  StringInputDefinition,
+  DeviceInputDefinition,
+  AttributeInputDefinition,
+  CommandInputDefinition,
+  SelectInputDefinition,
+  ColorInputDefinition,
+  CommandInput,
+  AttributeInput
+} from "../types";
+
+export type WidgetProps<T> = {
+  inputs: {
+    [K in keyof T]: T[K] extends NumberInputDefinition
+      ? number
+      : T[K] extends StringInputDefinition
+      ? string
+      : T[K] extends DeviceInputDefinition
+      ? {
+          name: string;
+          alias: string;
+        }
+      : T[K] extends AttributeInputDefinition
+      ? AttributeInput
+      : T[K] extends CommandInputDefinition
+      ? CommandInput
+      : T[K] extends SelectInputDefinition // TODO: infer U if possible
+      ? any
+      : T[K] extends ColorInputDefinition
+      ? string
+      : never
+  };
   t0: number;
+  mode: "run" | "edit" | "library";
   actualWidth: number;
   actualHeight: number;
-}
+};
