fddad75c89f61c48dbb33e13111892f0eba9fc92
hectorstudio <david.liu987@hotmail.com>
2019-10-01 14:46:40 +0200

    didnt mean to add  proxy settings

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,31 +1,30 @@
 const proxy = require("http-proxy-middleware");
 
 const proxies = {
+  "/testdb/socket": {
+    target: "http://localhost:22484",
   "/kitslab/socket": {
     target: "https://webjive-test.maxiv.lu.se",
     secure: false,
     changeOrigin: true,
     ws: true
   },
+  "/testdb/db": {
+    target: "http://localhost:22484",
   "/kitslab/db": {
     target: "https://webjive-test.maxiv.lu.se",
     secure: false,
     changeOrigin: true
   },
   "/auth": {
+    target: "http://localhost:22484",
     target: "https://webjive-test.maxiv.lu.se",
     secure: false,
     changeOrigin: true
   },
   "/dashboards": {
+    target: "http://localhost:22484",
     target: "https://webjive-test.maxiv.lu.se",
     secure: false,
     changeOrigin: true
   }
-};
-
-module.exports = function(app) {
-  Object.entries(proxies).forEach(([endpoint, setting]) => {
-    app.use(proxy(endpoint, setting));
-  });
-};
